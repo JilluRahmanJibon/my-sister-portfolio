f7d6a095803c7045456a8bfb996575bdf036ff71
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2022-12-23 08:14:17 +0600

    navbar and footer added

---
+++ src/Routes/Routes.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home/Home";
+import Root from "../Layout/Root";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/', element: <Root />, children: [
+        {path:'/',element:<Home/>}
+    ]}
+])
+export default router
